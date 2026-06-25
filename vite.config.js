be8967ae9221efd093156fc71dd27de98eb5b920
Throw <Throw@THROWS-PC>
2026-06-25 22:05:35 +0200

    Bust Pages asset cache

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash]-site[extname]",
+        chunkFileNames: "assets/[name]-[hash]-site.js",
+        entryFileNames: "assets/[name]-[hash]-site.js",
+      },
+    },
     sourcemap: false,
   },
 });
